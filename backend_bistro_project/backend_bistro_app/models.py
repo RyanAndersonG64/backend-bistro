38378b04ee4c9f3341540bb8d3a0d8b390839652
RyanAndersonG64 <ryan.anderson.g64@gmail.com>
2024-05-24 09:56:00 -0400

    demo commit

--- backend_bistro_project/backend_bistro_app/models.py
+++ backend_bistro_project/backend_bistro_app/models.py
@@ -1,10 +1,14 @@
-import datetime
+from datetime import datetime, timedelta, time
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
 class Allergen(models.Model):
     name = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class MenuItem(models.Model):
     item = models.TextField(max_length=100)
@@ -12,22 +16,40 @@ class MenuItem(models.Model):
     spice = models.SmallIntegerField(default=0)
     allergens = models.ManyToManyField(Allergen)
 
+    def __str__(self):
+        return self.item
+
 
 class Customer(models.Model):
     name = models.TextField(max_length=100)
     address = models.TextField(max_length=100)
+    phone_number = models.TextField(max_length=12)
+
+    def __str__(self):
+        return self.name
     
+
 
 class CustomerOrder(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     dine_in = models.BooleanField(default=True)
-    pickup_time = models.TimeField(auto_now_add=True)
-    paid = models.BooleanField(default=True)
+    pickup_time = models.TimeField()
+    order_item_2 = models.ManyToManyField(MenuItem, through='OrderItem')
+    paid = models.BooleanField(default=False)
     complete = models.BooleanField(default=False)
+
+    dine_status = 'Dine in'    
+    if dine_in == False:
+        dine_status = f'Takeout - {pickup_time}'
+
+    def __str__(self):
+        return f'{self.customer} - {self.dine_status}'
 
 
 class OrderItem(models.Model):
     item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     quantity = models.SmallIntegerField(default=0)
     customer_order = models.ForeignKey(CustomerOrder, on_delete=models.CASCADE, related_name='order_item')
-    
+
+    def __str__(self):
+        return f'{self.quantity} orders of {self.item}'
--- backend_bistro_project/backend_bistro_app/serializers.py
+++ backend_bistro_project/backend_bistro_app/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from .models import *
 
-class AllergenSerializer(serializers.Serializer):
+class AllergenSerializer(serializers.ModelSerializer):
     class Meta:
         model = Allergen
         fields = ['id', 'name']
 
 class MenuItemSerializer(serializers.ModelSerializer):
+    allergens = serializers.StringRelatedField(many=True, read_only=True)
     class Meta:
         model = MenuItem
         fields = ['id', 'item', 'category', 'spice', 'allergens']
@@ -14,12 +15,12 @@ class MenuItemSerializer(serializers.ModelSerializer):
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = ['id', 'name', 'address']
+        fields = ['id', 'name', 'address', 'phone_number']
 
 class CustomerOrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomerOrder
-        fields = ['id', 'customer', 'dine_in', 'pickup_time', 'paid', 'complete']
+        fields = ['id', 'customer', 'dine_in', 'pickup_time', 'order_item_2', 'paid', 'complete']
 
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
